2379b69b0cb34d20aa75ee16bc4348dcc507dcd2
Zachary Siefkes <zacharysiefkes@gmail.com>
2020-11-05 11:37:23 +1300

    Add GUI and date client writing timestamp to database.

--- TCPServer/src/DateClient.java
+++ TCPServer/src/DateClient.java
@@ -28,8 +28,7 @@ public class DateClient {
             "running the date service on port 9090:");*/
         int port = 9090;
         Socket s = new Socket(serverAddress, port);
-        BufferedReader input =
-            new BufferedReader(new InputStreamReader(s.getInputStream()));
+        BufferedReader input = new BufferedReader(new InputStreamReader(s.getInputStream()));
         String answer = input.readLine();
         //JOptionPane.showMessageDialog(null, answer);
         System.out.println(answer);
--- TCPServer/src/UserInterface.java
+++ TCPServer/src/UserInterface.java
@@ -1,6 +1,9 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.Socket;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
 
 import javafx.application.Application;
 import javafx.event.ActionEvent;
@@ -13,16 +16,17 @@ import javafx.stage.Stage;
 
 public class UserInterface extends Application {
 
-	// make a textfield and a button to submit
-	// textfield chooses port number
+	// client details
+//	private String clientIP;
+	private String timeStamp;
+//	private int clientPort;
+	private int localPort;
 
+	// JavaFX fields
 	TextField portInputField;
 	Button submitButton;
 	VBox pane;
 
-	/**
-	 *
-	 */
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		portInputField = new TextField();
@@ -30,31 +34,63 @@ public class UserInterface extends Application {
 		submitButton.setOnAction(new EventHandler<ActionEvent>() {
 			@Override
 			public void handle(ActionEvent arg0) {
-				try {
-					String serverAddress = "127.0.0.1";
-					// obtain port number from textfield and convert to integer
-					int port = Integer.parseInt(portInputField.getText());
-					Socket s = new Socket(serverAddress, port);
-					BufferedReader input = new BufferedReader(new InputStreamReader(s.getInputStream()));
-					String answer = input.readLine();
-					// JOptionPane.showMessageDialog(null, answer);
-					System.out.println(answer);
-					System.exit(0);
-					s.close();
-				} catch (Exception e) {
-					System.out.println("Error: " + e);
+				timeStamp = connectToDateServer();
+				if (!timeStamp.equals("")) {
+					writeToDB(timeStamp);
 				}
 			}
 		});
-		
+
 		pane = new VBox();
 		pane.getChildren().add(portInputField);
 		pane.getChildren().add(submitButton);
 		Scene scene = new Scene(pane, 600, 400);
-		
+
 		primaryStage.setTitle("TCP Client");
 		primaryStage.setScene(scene);
 		primaryStage.show();
+	}
+
+	private String connectToDateServer() {
+		try {
+			String serverAddress = "127.0.0.1";
+			// obtain port number from textfield and convert to integer
+			localPort = Integer.parseInt(portInputField.getText());
+			Socket s = new Socket(serverAddress, localPort);
+			BufferedReader input = new BufferedReader(new InputStreamReader(s.getInputStream()));
+			String answer = input.readLine();
+			// JOptionPane.showMessageDialog(null, answer);
+//			System.out.println(answer);
+//			System.exit(0);
+			s.close();
+			// return data from server
+			return answer;
+		} catch (Exception e) {
+			System.out.println("Error: " + e);
+			return "";
+		}
+
+	}
+
+	private void writeToDB(String timeStamp) {
+// insert into client_details values ('192.168.56.1', 55252, Now() );
+		try {
+			String databaseUser = "dizach";
+			String databaseUserPass = "123";
+			Class.forName("org.postgresql.Driver");
+			
+			Connection connection = null;
+			String url = "jdbc:postgresql://localhost/testdb";
+			connection = DriverManager.getConnection(url, databaseUser, databaseUserPass);
+			
+			Statement s = connection.createStatement();
+			s.executeUpdate("insert into timestamps values ('" + timeStamp + "');");
+			connection.close();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.out.println("Login error: " + e.toString());
+		}
 	}
 
 	public static void main(String[] args) {
